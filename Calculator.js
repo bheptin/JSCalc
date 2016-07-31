c1a2f7dab2bd92f46f5a61992733235a02b4bc7d
Brian Heptinstall <bheptin@gmail.com>
2016-07-31 18:23:22 -0400

    Add files via upload

--- Calculator.js
+++ Calculator.js
@@ -1,11 +1,12 @@
 var text = document.querySelector("mainline");
-var numbers = document.querySelectorAll("#buttons");
+var numbers = document.querySelectorAll(".numbers");
 for(var i = 1; i < numbers.length; i++){
 
   var calcNumbers=text[i];
   calcNumbers.addEventListener("click", function(event){
     var clickedItem = event.target;
-    var text = clickedItem.innerText;
-    var newItem = document.createElement("numbers")
+    var numbers = clickedItem.innerText;
+    var text = document.createElement("numbers")
   })
+
 }
